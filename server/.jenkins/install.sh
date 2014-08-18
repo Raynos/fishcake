e8799b046dfd0ad7251765f17f0fa848129a1b11
Raynos <raynos2@gmail.com>
2014-08-18 04:21:22 -0700

    fix jenkins stuff

---
+++ server/.jenkins/install.sh
@@ -0,0 +1,10 @@
+npm --registry=http://archive.uber.com/npm install
+
+cd ..
+cd business/ && npm --registry=http://archive.uber.com/npm install
+cd ..
+cd endpoints/auth/ && npm --registry=http://archive.uber.com/npm install
+cd ..
+cd endpoints/device/ && npm --registry=http://archive.uber.com/npm install
+cd ..
+cd endpoints/health/ && npm --registry=http://archive.uber.com/npm install
